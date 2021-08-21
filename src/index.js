7b9c22adc7fc5179f320fbad1bb97e5e4de3fbb8
Evgeny <hatehateme@bk.ru>
2021-08-21 17:50:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length < 1) { 
+      return 0;
+    } else {
+      var min = array[0]; array.forEach (num =>  {
+        if (num < min) {
+          min = num;
+        }
+      });
+  return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length < 1) { 
+    return 0;
+  } else {
+    var max = array[0]; array.forEach (num =>  {
+      if (num > max) {
+        max = num;  
+      }
+    });
+  
+  return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length < 1) { 
+    return 0;
+  } else {
+    return array.reduce((index, total) => total + index) / 
+    array.length;
+  } 
 }
